b2d935becadf5133e47c773fec6a39f0252e6090
Korapat Leelabandit <mio.cubrick@gmail.com>
2021-04-18 02:50:46 +0700

    เพิ่ม route api สำหรับ jwt :apple:

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Auth Controller
+Route::get('login', [AuthController::class, 'login']);
+Route::get('register', [AuthController::class, 'register']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// After Login Group Controller
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('logout', [UserController::class, 'logout']);
+    Route::get('refresh', [UserController::class, 'refresh']);
+    Route::get('me', [UserController::class, 'me']);
 });
